0f2a1fc0d463d8fca674ae0404caf4f33fd8fc26
Hemanth622004 <126446141+Hemanth622004@users.noreply.github.com>
2024-06-19 14:33:59 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-// const parent = React.createElement("div",
-//     {id:"parent"},
-//     [React.createElement("div",
-//         {id:"child"},
-//         React.createElement("h1",
-//             {},
-//             "Thish ishsj")),React.createElement("div",
-//                 {id:"child"},
-//                 React.createElement("h1",
-//                     {},
-//                     "Thish ishsj"))])
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);
+const parent = React.createElement("div",
+    {id:"parent"},
+    [React.createElement("div",
+        {id:"child"},
+        React.createElement("h1",
+            {},
+            "Thish ishsj")),React.createElement("div",
+                {id:"child"},
+                React.createElement("h1",
+                    {},
+                    "Thish ishsj"))])
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
